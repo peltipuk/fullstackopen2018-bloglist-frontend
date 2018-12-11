6604bd742bd5a177d49717d1debf600c9a465a19
Lauri Vanhatalo <lauri.vanhatalo@nokia.com>
2018-12-11 15:49:06 +0200

    Exercise 5.13

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -11,7 +11,7 @@ describe.only('<SimpleBlog />', () => {
     }
 
     const simpleBlogComponent = shallow(<SimpleBlog blog={blog} />)
-    console.log(simpleBlogComponent.debug())
+    //console.log(simpleBlogComponent.debug())
 
     const titleAndAuthorDiv = simpleBlogComponent.find('.titleAndAuthor')
     expect(titleAndAuthorDiv.text()).toContain(blog.title)
@@ -19,5 +19,23 @@ describe.only('<SimpleBlog />', () => {
 
     const likesDiv = simpleBlogComponent.find('.likes')
     expect(likesDiv.text()).toContain(blog.likes.toString())
+  })
+
+  it('clicking like button calls event handler', () => {
+    const blog = {
+      title: 'My superb blog',
+      author: 'Joan Jefferson',
+      likes: 12,
+    }
+    const mockHandler = jest.fn()
+
+    const simpleBlogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+    //console.log(simpleBlogComponent.debug())
+
+    const likeButton = simpleBlogComponent.find('button')
+    likeButton.simulate('click')
+    likeButton.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
